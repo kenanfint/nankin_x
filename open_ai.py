d6602028522ab7530fa525c164f90e61c14d6156
Kenan Fintelman <kenanfintelman123@hotmail.com>
2025-04-24 16:34:27 -0300

    feat: creating send tweet feat, and connecting to Open AI

---
+++ open_ai.py
@@ -0,0 +1,15 @@
+import os
+import config
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=config.OPEN_AI_API_KEY,
+)
+
+response = client.responses.create(
+    model="gpt-4o",
+    instructions="You are a coding assistant that talks like a pirate.",
+    input="How do I check if a Python object is an instance of a class?",
+)
+
+print(response.output_text)

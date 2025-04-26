4145af9939db056188915ee124b8475f4554f9fc
Kenan Fintelman <kenanfintelman123@hotmail.com>
2025-04-26 17:58:15 -0300

    feat: telegram stuff

--- open_ai.py
+++ open_ai.py
@@ -7,9 +7,8 @@ client = OpenAI(
 )
 
 response = client.responses.create(
-    model="gpt-4o",
-    instructions="You are a coding assistant that talks like a pirate.",
-    input="How do I check if a Python object is an instance of a class",
+    model="gpt-4.1",
+    input="Qual o custo em dolares por token do gpt-4.1?"
 )
 
 print(response.output_text)
